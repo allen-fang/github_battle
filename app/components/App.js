b31b222bc3ed2bf4d21b4ae172369a83787aa197
Allen Fang <allenfang08@gmail.com>
2017-06-23 17:43:10 -0700

    nav bar added

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Popular from "./Popular.js"
+import {BrowserRouter, Route} from "react-router-dom";
+import Nav from "./Nav.js";
 
 // components: UI, state, lifecycle events
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className="container ">
-				<Popular />
-			</div>
+			<BrowserRouter>
+				<div className="container ">
+					<Nav />
+					<Route path="/popular" component={Popular} />
+				</div>
+			</BrowserRouter>
+			
 		)
 	}
 }
