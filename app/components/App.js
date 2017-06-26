f60a6e43d441f09931876bfcf5565d21a7822049
Allen Fang <allenfang08@gmail.com>
2017-06-26 10:53:46 -0700

    routed all the nav bar items

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Popular from "./Popular.js"
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Nav from "./Nav.js";
 import Home from "./Home.js";
+import Battle from "./Battle.js"
 
 // components: UI, state, lifecycle events
 
@@ -12,8 +13,14 @@ class App extends React.Component {
 			<BrowserRouter>
 				<div className="container ">
 					<Nav />
-					<Route exact path="/" component={Home} />
-					<Route path="/popular" component={Popular} />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/battle" component={Battle} />
+						<Route path="/popular" component={Popular} />
+						<Route render={() => {
+							return <p>Error 404 - Not Found</p>
+						}} />
+					</Switch>
 				</div>
 			</BrowserRouter>
 			
